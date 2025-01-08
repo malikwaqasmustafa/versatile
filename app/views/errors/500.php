c6aabbb072c903002e3aeea68da064745e6182f0
Waqas Mustafa <malik.waqas.09@hotmail.com>
2025-01-08 20:00:14 +0500

    latest
    latest

--- app/views/errors/500.php
+++ app/views/errors/500.php
@@ -3,7 +3,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>500 - Server Error</title>
+    <title>500 - Server Error | <?= $title ?? 'Versatile PHP' ?></title>
 </head>
 <body>
 <h1>500 - Internal Server Error</h1>
